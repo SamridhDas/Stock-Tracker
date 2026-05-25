2fc9b83c3ea0c3bcfe16a66d68be67ad0b97baac
Samridh Das <samridhdas@gmail.com>
2026-05-25 11:57:09 +0000

    Added stock visualisations and moving averages

--- main.py
+++ main.py
@@ -1,15 +1,50 @@
 import yfinance as yf
-ticker=input("Enter stock ticker: ")
+import matplotlib.pyplot as plt
+ticker=input("Enter stock ticker: ").upper()
 stock=yf.Ticker(ticker)
-data=stock.history(period="1d")
+days=input("Enter Number of Days: ")
+period=days+"d"
+data=stock.history(period=period)
 latest_open=data["Open"].iloc[-1]
 latest_close=data["Close"].iloc[-1]
 perchange=((latest_close-latest_open)/latest_open)*100
-print(ticker)
-print(f"Latest Price: {latest_close:.2f}")
-print(f"Daily Change: {perchange:.2f}%")
+per_sign=""
 if perchange>0:
-    print("Green")
+    per_sign="+"
 else:
-    print("Red")
+    per_sign=""
+moving_average=data["Close"].rolling(window=3).mean()
+print("===== STOCK ANALYSIS =====")
+print("--------------------------")
+print(f"\nTicker: {ticker}\n")
+print("--- PRICE DATA ---")
+print(f"Latest Price: ${latest_close:.2f}")
+print("Daily Change: "+per_sign+f"{perchange:.2f}%\n")
+print("--- ANALYTICS ---")
+average_close=data["Close"].mean()
+print(f"Average Closing Price: ${average_close:.2f}")
+max_closing_price=data["Close"].max()
+min_closing_price=data["Close"].min()
+print(f"Highest Closing Price: ${max_closing_price:.2f}")
+print(f"Lowest Closing Price: ${min_closing_price:.2f}")
+status=""
+if perchange>0:
+    status="Bullish"
+elif perchange<0:
+    status="Bearish"
+else:
+    status="No Change"
+print(f"\nMarket Status: {status}")
+plt.figure(figsize=(12,8))
+plt.grid()
+plt.plot(data.index,data["Close"],label="Closing Price")
+plt.plot(data.index,moving_average,label="3-day Moving Average")
+plt.legend()
+plt.title(f"{ticker} Closing Prices")
+plt.xlabel("Date")
+plt.xticks(rotation=45)
+plt.ylabel("Closing Price")
+plt.savefig("stock_chart.png")
+
+
 
